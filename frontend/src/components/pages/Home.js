9b0b5e61e4f1416bb6c2075f419f4adcc6967ff4
Briena <brienaqwe@gmail.com>
2025-06-21 15:26:32 -0300

    ajustes no design da Home

--- frontend/src/components/pages/Home.js
+++ frontend/src/components/pages/Home.js
@@ -1,7 +1,5 @@
 import Carousel from 'react-bootstrap/Carousel';
-
 import { Link } from 'react-router-dom';
-
 import { useEffect, useState } from 'react';
 
 import styles from './Home.module.css';
@@ -31,15 +29,18 @@ function Home() {
     <section className={styles.home_container}>
       <div className={styles.principal}>
         <div className={styles.imagens}>
-          <img className={styles.arbusto_verde} src={arbusto_verde} />
-          <img className={styles.dog} src={dog} />
-          <img className={styles.arbusto_verde_musgo} src={arbusto_verde_musgo} />
-          <img className={styles.racao} src={racao} />
+          <img className={styles.arbusto_verde} src={arbusto_verde} alt="arbusto verde" />
+          <img className={styles.dog} src={dog} alt="dog" />
+          <img className={styles.arbusto_verde_musgo} src={arbusto_verde_musgo} alt="arbusto verde musgo" />
+          <img className={styles.racao} src={racao} alt="ração" />
         </div>
         <div className={styles.prim_part}>
           <h1>Comida para Pet</h1>
           <p>Aqui temos comida para pet e muito mais!</p>
-          <a className={styles.button_buscar} href="/">Buscar Produtos</a>
+          <div className={styles.botoes}>
+            <Link className={styles.button_buscar} to="/alimentos">Buscar Produtos</Link>
+            <Link className={styles.button_servicos} to="/servicos">Serviços</Link>
+          </div>
         </div>
       </div>
       <div className={styles.cont_carousel}>
@@ -57,18 +58,16 @@ function Home() {
         </Carousel>
 
         <div className={styles.secao_produtos}>
-        <h2 className={styles.titulo_secao}>Destaques de Higiene</h2>
-        <CarouselProdutos produtos={produtosHigiene.slice(0, 5)} />
+          <h2 className={styles.titulo_secao}>Destaques de Higiene</h2>
+          <CarouselProdutos produtos={produtosHigiene.slice(0, 5)} />
         </div>
         <div className={styles.secao_produtos}>
-        <h2 className={styles.titulo_secao}>Destaques de Acessórios</h2>
-        <CarouselProdutos produtos={produtosAcessorios.slice(0, 5)} />
+          <h2 className={styles.titulo_secao}>Destaques de Acessórios</h2>
+          <CarouselProdutos produtos={produtosAcessorios.slice(0, 5)} />
         </div>
-        
-
       </div>
       <div className={styles.banner}>
-        <img src={banner_conheca} />
+        <img src={banner_conheca} alt="Banner Conheça Our" />
         <Link className={styles.button_peca} to="/servicos">Peça agora</Link>
       </div>
     </section>
